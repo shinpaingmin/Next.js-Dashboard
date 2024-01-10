821aa22c98bf0764d6ee603e6f2349610b589b0a
shinpaingmin <shinpaingmin@gmail.com>
2024-01-10 18:17:51 +0630

    added delete server action

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -29,7 +29,7 @@ export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
   return (
     <>
-      <form action={}>
+      <form action={deleteInvoiceWithId}>
         <button className="rounded-md border p-2 hover:bg-gray-100">
           <span className="sr-only">Delete</span>
           <TrashIcon className="w-5" />
