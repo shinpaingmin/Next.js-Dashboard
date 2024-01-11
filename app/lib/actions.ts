433bff032b01a2f17352edfc54361e7be32f017c
shinpaingmin <shinpaingmin@gmail.com>
2024-01-11 11:57:46 +0630

    added user input validation for create-form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,40 +7,70 @@ import { redirect } from "next/navigation";
 
 const formSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer.'     // user-friendly error message
+    }),
+    amount: z.coerce
+     .number()
+     .gt(0, 'Please enter an amount greater than $0.'),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status.'
+    }),
     date: z.string()
 });
 
 const CreateInvoice = formSchema.omit({ id: true, date: true });
 const UpdateInvoice = formSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+    errors?: {
+        customerId?: string[],
+        amount?: string[],
+        status?: string[],
+    };
+    message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
     // for multiple entries
     // const rawFormData = Object.fromEntries(formData.entries());
     // console.log(rawFormData);
 
-    const { customerId, amount, status } = CreateInvoice.parse({
+    // validating data
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status')
-    })
+    });
 
+    
+    // If form validation fails, return errors early. Otherwise, continue.
+    if(!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.'
+        }
+    }
+
+    // Prepare data for insertion into the database
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
+    // Insert data into the database
     try {
         await sql `
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error) {
+        // If a database error occurs, return a more specific error.
         return {
             message: 'Database Error: Failed to Create Invoice.'
         }
     }
 
+    // Revalidate the cache for the invoices page and redirect the user.
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');    // must be outside of trycatch block
 }
@@ -69,7 +99,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
     try {
         await sql `
         DELETE FROM invoices WHERE id = ${id}
